8bbe3758eb2ae5e1f447ee6269a09264ed2c845c
lxjhk <lxjpub@gmail.com>
2016-09-11 01:56:09 +0800

    update

--- src/Chaincode.go
+++ src/Chaincode.go
@@ -148,6 +148,9 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 	//ADD
 	time, errGetTxTimestamp :=  stub.GetTxTimestamp()
 	payload, errpayload := stub.GetPayload();
+	if false {
+		return errGetTxTimestamp, errpayload
+		}
 	//time_string, errTimestampString := TimestampString(time)
 	jsonResp := "{" + string(payload[:]) + "," + string(Avalbytes) + "}"
 	return nil, errors.New(jsonResp)
